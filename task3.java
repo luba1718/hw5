05f97a3ec9be45ed0038e75bafe53cff5ecb3f04
Luba <bart6891@yandwx.ru>
2023-04-04 16:16:21 +0300

    3

---
+++ task3.java
@@ -0,0 +1,56 @@
+public class task3 {
+    private int[][] board;
+    private int size;
+
+    public task3(int size) {
+        this.board = new int[size][size];
+        this.size = size;
+    }
+
+    private boolean isSafe(int row, int col) {
+        for (int i = 0; i < size; i++) {
+            if (board[row][i] == 1 || board[i][col] == 1) {
+                return false;
+            }
+        }
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean placeQueens(int col) {
+        if (col == size) {
+            return true;
+        }
+        for (int i = 0; i < size; i++) {
+            if (isSafe(i, col)) {
+                board[i][col] = 1;
+
+                if (placeQueens(col + 1)) {
+                    return true;
+                }
+
+                board[i][col] = 0;
+            }
+        }
+        return false;
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        task3 board = new task3(8);
+        board.placeQueens(0);
+        board.printBoard();
+    }
+}
